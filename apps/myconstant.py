a8d9ed63a8f3572ac62550dc17d82c27aeb1143f
mahesh-brg <130646029+mahesh-brg@users.noreply.github.com>
2023-12-29 06:01:06 +0530

    Updated myconstant

--- apps/myconstant.py
+++ apps/myconstant.py
@@ -19,3 +19,18 @@ JOB_STATUS_IN_PROGRESS= '2'
 JOB_STATUS_NOT_STARTED = '3'
 JOB_STATUS_FAILED = '4'
 ProductVersion= "2023.8"
+
+QBO_CI='ABpWOUWtcEG1gCun5dQbQNfc7dvyalw5qVF97AkJQcn5Lh09o6'
+QBO_CS='LepyjXTADW592Dq5RYUP8UbGLcH5xtqDQhrf2xJN'
+QBO_Token="https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+QBO_REDIRECT='http://localhost:5000/data_access'
+QBO_Authorization='https://appcenter.intuit.com/connect/oauth2'
+
+Accountright_uri="http://localhost:8080/AccountRight"
+
+Queue_URI='https://sqs.your-aws-region.amazonaws.com/074206019712/MmcSelfServiceEmail'
+
+aws_access_key_id1 = 'AKIARCRYGACABFYEF6O3'
+aws_secret_access_key1 = 'o3ZGbTXjjdSmZGXMhRsV6O1R8Fl7ZUhnsBTGR1Tr'
+region_name1 = 'ap-south-1'
+
